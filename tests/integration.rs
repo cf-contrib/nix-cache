b4078eefc4dfd122144b0d328d9a9c13a92de46f
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-05-14 17:46:26 +0400

    style: remove blank line in integration test
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,14 +1,11 @@
 use narinfo::NixCacheInfo;
-use tokio::test;
 
-#[test]
+#[tokio::test]
 async fn test_get_nix_cache_info() {
     let response = reqwest::get("http://127.0.0.1:8787/nix-cache-info")
         .await
         .expect("the request failed");
-
     assert_eq!(response.status(), 200);
-
     let data = response.text().await.expect("the body failed");
     let info = NixCacheInfo::parse(&data).expect("the response failed");
     assert_eq!(info.store_dir, "/nix/store");
